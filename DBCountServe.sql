493a178c220c0029b94e5e760df4f17c9f452661
David Santo <david.santo@hotmail.com.br>
2023-11-27 13:30:56 -0300

    Script do banco

---
+++ DBCountServe.sql
@@ -0,0 +1,27 @@
+use countserve;
+
+create table cardapio (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(25) NOT NULL,
+    acompanhamento VARCHAR(50),
+    foto LONGBLOB,
+    data DATE NOT NULL
+);
+
+create table pessoa (
+	id int primary key not null auto_increment,
+    nome varchar(50) not null,
+    dt_nasc date not null,
+    turm_num int,
+    perfil enum('administrador', 'professor', 'aluno') not null,
+    user_id INT REFERENCES users(id)
+);
+
+CREATE TABLE agenda (
+    cardapio_id INT,
+    pessoa_id INT,
+    status ENUM('confirmado', 'cancelado'),
+    PRIMARY KEY (cardapio_id, pessoa_id),
+    FOREIGN KEY (cardapio_id) REFERENCES cardapio(id),
+    FOREIGN KEY (pessoa_id) REFERENCES pessoa(id)
+);
